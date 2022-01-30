0098d4e85435b68faacd4a5962ec4418314350f7
Soroush-Pasandideh <86403507+Soroush-Pasandideh@users.noreply.github.com>
2022-01-30 22:48:09 +0330

    Add files via upload
    adding index method

---
+++ src/src/Main.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Rope> AllRopes = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            String line = scanner.nextLine();
+            String[] splited = line.split(" ", 3);
+
+            switch (splited[1]) {
+                case "new":
+                    Rope rope = new Rope(splited[2]);
+                    AllRopes.add(rope);
+                    break;
+                case "status":
+                    int count = 1;
+                    for (Rope tempRope : AllRopes) {
+                        System.out.print((count++) + ". ");
+                        tempRope.printRope(tempRope.headNode);
+                        System.out.println();
+                    }
+                    break;
+                case "concat":
+                    Rope s1 = null;
+                    Rope s2 = null;
+                    int firstIndex=splited[2].charAt(0)-49;
+                    int secondIndex=splited[2].charAt(2)-49;
+                    if (AllRopes.size()>=secondIndex) {
+                        for (int i = 0; i < AllRopes.size(); i++) {
+                            if (i == firstIndex) {
+                                s1 = AllRopes.get(i);
+                            }
+                            if (i == secondIndex) {
+                                s2 = AllRopes.get(i);
+                            }
+                        }
+                        AllRopes.remove(firstIndex);
+                        AllRopes.add(firstIndex, Rope.concat(s1, s2));
+                        AllRopes.remove(secondIndex);
+                    }
+                    break;
+                case"index":
+                    int ropeNumber=splited[2].charAt(0)-49;
+                    int index=splited[2].charAt(2)-49;
+                    Rope wantedRope=null;
+                    if (AllRopes.size()>=ropeNumber) {
+                        for (int i = 0; i < AllRopes.size(); i++) {
+                            if (i == ropeNumber) {
+                                wantedRope = AllRopes.get(i);
+                            }
+                        }
+                    }
+                    wantedRope.index(index);
+            }
+        }
+    }
+}
---
+++ src/src/Rope.java
@@ -0,0 +1,134 @@
+import java.util.ArrayList;
+
+public class Rope {
+    Node headNode;
+    static ArrayList<String> ropeToString = new ArrayList<>();
+
+    public Rope(String string) {
+        headNode = new Node(0, null);
+        Node ptr = headNode;
+        String[] sentence = string.split(" ");
+        // hazf " haye aval va akhar
+        sentence[0] = sentence[0].substring(1);
+        sentence[sentence.length - 1] = sentence[sentence.length - 1].substring(0, sentence[sentence.length - 1].length() - 1);
+        //ezafe kardane space be hameye khoone ha joz akhari
+        for (int i = 0; i < sentence.length - 1; i++) {
+            sentence[i] = sentence[i].concat(" ");
+        }
+        makeRope(sentence, ptr);
+    }
+
+    // NEW
+    public void makeRope(String[] sentence, Node ptr) {
+        while (sentence.length >= 2) {
+            ptr.size = numberOfLeftChars(sentence);
+            ptr.lNode = new Node(0, null);
+            ptr.rNode = new Node(0, null);
+            String[] leftHalf = leftHalf(sentence);
+            String[] rightHalf = righthalf(sentence);
+            makeRope(leftHalf, ptr.lNode);
+            makeRope(rightHalf, ptr.rNode);
+            break;
+        }
+        if (sentence.length == 1) {
+            ptr.string = sentence[0];
+            ptr.size = sentence[0].length();
+        }
+    }
+
+    public String[] leftHalf(String[] sentence) {
+        String[] leftHalf = new String[(int) Math.ceil((float) sentence.length / 2)];
+        for (int i = 0; i < Math.ceil((float) sentence.length / 2); i++) {
+            leftHalf[i] = sentence[i];
+        }
+        return leftHalf;
+    }
+
+    public String[] righthalf(String[] sentence) {
+        String[] righthalf = new String[sentence.length / 2];
+        for (int j = 0, i = (int) Math.ceil((float) sentence.length / 2); j < sentence.length / 2; j++, i++) {
+            righthalf[j] = sentence[i];
+        }
+        return righthalf;
+    }
+
+    public int numberOfLeftChars(String[] sentence) {
+        // be dast avardane tedad character haye nimeye aval araye baraye size ptr
+        int size = 0;
+        for (int i = 0; i < Math.ceil((float) sentence.length / 2); i++) {
+            size += sentence[i].length();
+        }
+        return size;
+    }
+
+    // STATUS
+    public void printRope(Node ptr) {
+        if (ptr != null) {
+            printRope(ptr.lNode);
+            if (ptr.string != null) {
+                System.out.print(ptr.string);
+            }
+            printRope(ptr.rNode);
+        }
+    }
+
+    public static void ropeToStringArr(Node head) {
+        if(head!=null){
+            ropeToStringArr(head.lNode);
+            if (head.string!=null){
+                ropeToString.add(head.string);
+            }
+            ropeToStringArr(head.rNode);
+        }
+    }
+
+    public static Rope concat(Rope s1, Rope s2) {
+        ropeToString.clear();
+        ropeToStringArr(s1.headNode);
+        ropeToStringArr(s2.headNode);
+        return new Rope(toString(ropeToString));
+
+    }
+
+    public static String toString(ArrayList<String> arr){
+        String result="\"";
+        for (String s:arr) {
+            result+=s;
+        }
+        result+="\"";
+        return result;
+    }
+
+    public void index(int index){
+        ropeToString.clear();
+        char letter = 0;
+        ropeToStringArr(this.headNode);// array ro por kardim
+        for (int i = 0; i < ropeToString.size(); i++) {
+            if (index<ropeToString.get(i).length()){
+                letter=ropeToString.get(i).charAt(index);
+                break;
+            }
+            else {
+                index-=ropeToString.get(i).length();
+            }
+        }
+        System.out.println(letter);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
